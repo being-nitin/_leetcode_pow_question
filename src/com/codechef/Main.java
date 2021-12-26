f3dc094f16888ca2eb3dfb87bf1c3c1a3652a8d6
Nitin <nsinghboss02@gmail.com>
2021-12-26 10:35:19 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,16 @@
+package com.codechef;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	leetcode problem to find the power of a a decimal number:
+	 */
+        double power = pow(2.00000,10);
+        System.out.println(power);
+    }
+    public static double pow(double x, int n){
+        double pow1= Math.pow(x,n);
+        return pow1;
+    }
+}
